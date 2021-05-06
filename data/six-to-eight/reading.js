66fe18e20a0c3d29897c5c47de52839a259fb70f
vidhi-mody <vidhimody6@gmail.com>
2021-05-06 11:40:31 +0530

    edit: questions

--- data/six-to-eight/reading.js
+++ data/six-to-eight/reading.js
@@ -1,7 +1,7 @@
 module.exports = [
   "Is the child able to read a short passage / story smoothly and accurately (is able to read unfamiliar words, not guess read them)?",
   "Is the child’s reading of words and sentences accurate (without reversals, pauses, mispronunciations or frequent errors in reading aloud)?",
-  "Can the child correctly identify the beginning, middle and ending of a story(introduction, body & conclusion) and re-tell thepart asked for (What happened at the beginning / end of the story)?",
+  "Can the child correctly identify the beginning, middle and ending of a story(introduction, body & conclusion) and re-tell the part asked for (What happened at the beginning / end of the story)?",
   "Can the child accurately read 4-letter and 2 syllable words in a sentence (spin /picnic)?",
   "Is the child able to identify the parts of a sentence (nouns, verbs, adjectives, pronouns and articles)?",
   "Is the child able to read a story and identify: the main characters, the author’s message and change the conclusion / give a different ending to the story?",
--- data/six-to-eight/spelling.js
+++ data/six-to-eight/spelling.js
@@ -1,7 +1,7 @@
 module.exports = [
   "Can the child spell words and sentences accurately? (no reversals, omissions, additions or other spelling errors)?",
   "Can the child accurately spell 4 letter & 2 syllable words to dictation?",
-  "Is the child able to spell what he / she has written (own writing)?",
+  "Is the child able to spell words that follow patterns learned previously (spelling patterns in word families like tr-ain, g-ain, m-ain, gr-ain)?",
   "Is the child happy to complete written tasks without showing any anger/resistance/frustration?",
   "Is the child able to complete written tasks?",
   "Does the child find it easy to use punctuation and capitalization in written tasks?",
